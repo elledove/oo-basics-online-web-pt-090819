c5eb76e3fa32dec96c3da11d1ba6a094d00dd2fa
Lauren Dove <elledoveinc@gmail.com>
2019-09-27 01:18:15 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,11 +9,12 @@ class Shoe
    end
    
    def cobble 
-     #if condition == "new"
+     condition = "new"
+     if condition == "new"
     puts  "Your shoe is as good as new!"
-   #else 
-     # "old"
-  #end
+   else 
+     puts "old"
+  end
    
 end
    
